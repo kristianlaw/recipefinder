efb02e4688172cef2b1f5cce4871ebe9ea7c8a08
kristianlaw <lawkristian@hotmail.com>
2020-09-17 21:31:24 +0300

    Done

--- App.js
+++ App.js
@@ -1,15 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button, TextInput, Alert, Image } from 'react-native';
 
 export default function App() {
+  const [aines, setAines] = useState('');
+  const [resepti, setResepti] = useState([]);
+
+
+  const getResepti = () => {
+  fetch('http://www.recipepuppy.com/api/?i=' + aines)
+  .then(response => response.json())
+  .then(data => setResepti(data.results))
+  .catch(err => console.error(err))
+}
+
+    const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <View style={styles.listcontainer}>
+    <FlatList
+        style={{marginLeft : "1%"}}
+        ItemSeparatorComponent={listSeparator}
+        data={resepti}
+        keyExtractor={item => item.results}
+        renderItem={({item}) =>
+          <View style={{flex:1, flexDirection: 'row', fontSize: 10}}>
+            <Image source = {{ uri: item.thumbnail }} style={styles.imageView} />
+            <Text>{item.title}</Text>
+          </View>
+        } />
     </View>
-  );
+
+    <TextInput
+        style={{fontSize: 18, width: 200}}
+        value={aines}
+        placeholder="Ingredient"
+        onChangeText={(aines) => setAines(aines)}
+    />
+
+    <Button title="Find recipe" onPress={getResepti} />
+  </View>
+);
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +67,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listcontainer: {
+    height: '80%'
+  },
+  imageView: {
+    width: '20%',
+    height: 90 ,
+    margin: 7,
+    borderRadius : 7
+  }
 });
